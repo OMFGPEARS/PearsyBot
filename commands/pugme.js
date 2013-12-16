9bd454c17accce5a5d201538236571bfc60db2c1
Brian Hong <hong.brian.j@gmail.com>
2013-12-15 18:41:24 -0800

    eval evil replacement

--- commands/pugme.js
+++ commands/pugme.js
@@ -7,10 +7,8 @@ exports.matchStart = false;
 exports.handler = function(data) {
  request('http://pugme.herokuapp.com/random', function cbfunc(error, response, body) {
     if (!error && response.statusCode == 200) {
-        var formatted = eval('(' + body + ')');
-
         try {
-            bot.speak(formatted.pug);
+            bot.speak(JSON.parse(body).pug);
         } catch (e) {
             console.log(e);
             bot.chat('I CAN\'T?');
