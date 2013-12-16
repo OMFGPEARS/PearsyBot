200cedfff003b1be593e3ec139eac71161536b4c
Matt Thompson <matthewaaronthompson@gmail.com>
2013-12-16 17:52:40 -0600

    experimental quote thing

---
+++ commands/quote.js
@@ -0,0 +1,15 @@
+exports.names = ['quote'];
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.handler = function(data) {
+	if (data.message.substring(0, 5).match(/pears/)){
+	  var person = data.message.slice(12);
+	} else {
+	  var person = data.message.slice(6);
+	}
+
+    db.get('SELECT quote from QUOTES WHERE speaker="'+person+'" ORDER BY RANDOM() LIMIT 1', function(err, row) {
+       if (row) bot.chat(row['quote']);
+    })
+};
