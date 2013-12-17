d0d5ad1b15bdb1512c53b66da7ebc0fa436680d8
Brian Hong <hong.brian.j@gmail.com>
2013-12-16 21:06:08 -0800

    ugly flags for similar and lookup using ddj without breaking it into another module

--- commands/ddg.js
+++ commands/ddg.js
@@ -2,19 +2,40 @@ var DDG = require('node-ddg-api').DDG;
 var ddg = new DDG('omfgpears');
 
 // Pugs on demand
-exports.names = ['lookup'];
+exports.names = ['lookup', 'similar'];
 exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data) {
-    if (data.message.substring(0, 5).match(/pears/)){
-      var subject = data.message.slice(13);
-    } else {
-      var subject = data.message.slice(7);
+    var prefix = 0;
+    var similar = 0;
+    
+    if (data.message.substring(0, 5).match(/pears/)){ prefix = 1;}
+    if (data.message.substring(8, 12).match(/[ ]*similar/) || data.message.substring(0, 7).match(/similar/) ){ similar = 1;}
+    
+    switch(prefix){
+        case 1:
+            if (similar == 1){ 
+                var subject = data.message.slice(14);
+            }else{
+                var subject = data.message.slice(13);
+            }
+        break;
+        case 0:
+            if (similar == 1){ 
+                var subject = data.message.slice(8);
+            }else{
+                var subject = data.message.slice(7);
+            }
+        break;
+        default:
+            var subject = data.message.slice(7);
     }
+    console.log(subject);
     ddg.instantAnswer(subject, {skip_disambig: '1'}, function(err, response){
-        console.log(response);
-        if (response.Definition){
+        if (similar == 1){
+            bot.chat(response.RelatedTopics[0].FirstURL);
+        }else if (response.Definition){
             bot.chat(response.Definition);
         }else if (response.AbstractURL != ""){
             bot.chat(response.AbstractURL);
