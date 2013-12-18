20da6305ea016737b03335b6487082de7e612c08
Brian Hong <hong.brian.j@gmail.com>
2013-12-18 13:23:25 -0800

    last played date human readability

--- commands/lastplayed.js
+++ commands/lastplayed.js
@@ -1,14 +1,18 @@
-exports.names = ['.lastplayed', 'lastplayed'];
+exports.names = ['last played', '.lastplayed', 'lastplayed'];
 exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
     db.get('SELECT username, started, upvotes, downvotes, snags FROM (SELECT userid, started, upvotes, downvotes, snags FROM PLAYS WHERE songid = ? ORDER BY started DESC LIMIT 1) a INNER JOIN USERS ON a.userid = USERS.userid', room.media.id, function(error, row) {
-        console.log(room.media.id);
-        console.log(error);
-        console.log(row);
         if (row != null) {
-            bot.chat('This song was last played on ' + row['started'] + ' by ' + row['username'] + '(+' + row['upvotes'] + '/-' + row['downvotes'] + ')');
+            var utc_string = row['started'];
+            var local_string = (function(dtstr) {
+                var t0 = new Date(dtstr);
+                var t1 = Date.parse(t0.toUTCString().replace('GMT', ''));
+                var t2 = (2 * t0) - t1;
+                return new Date(t2).toString();
+            })(utc_string);
+            bot.chat('I heard this song last ' + local_string + ' by ' + row['username'] + ' and it got ' + row['upvotes'] + ' woot and ' + row['downvotes'] + ' meh).');
         } else {
             bot.chat('This is the first time this song has been played in here.');
         }
