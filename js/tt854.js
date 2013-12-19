978cbc681a352cdec6d397e3111169779efe8b7b
Matt Thompson <matthewaaronthompson@gmail.com>
2013-12-18 21:42:47 -0600

    tt854 bkmrklt update

--- js/tt854.js
+++ js/tt854.js
@@ -1,27 +1,53 @@
 initAPIListeners();
+var wooting = false;
+
 
 function chatcallback(data) {
 
     if (data.message == '.christmas1') {
-        $('body').css('background-image', 'url(http://i.imgur.com/2Q89Rn2.png)');
-        $('#playback .background').hide();
-        $('#playback-container').css('border', '2px solid #4D4D4D');
-
+       changebg('http://i.imgur.com/2Q89Rn2.png');
     } else if (data.message == '.christmas2') {
-
-        $('body').css('background-image', 'url(http://i.imgur.com/P4GVhF4.png)');
-        $('#playback .background').hide();
-        $('#playback-container').css('border', '2px solid #4D4D4D');
+       changebg('http://i.imgur.com/P4GVhF4.png')
 
     } else if (data.message == '.christmas3') {
-
-        $('body').css('background-image', 'url(http://i.imgur.com/M0CeHah.png)');
-        $('#playback .background').hide();
-        $('#playback-container').css('border', '2px solid #4D4D4D');
+        changebg('http://i.imgur.com/M0CeHah.png');
     }
 
 }
 
+function commandcallback(value) {
+
+  if (value == '/autowoot'){
+  wooting = true;
+  API.chatLog('Autowooting has been activated, dawg.',false);
+  wootSong();
+  } else if (value == '/kill autowoot') {
+  wooting = false;
+  API.chatLog('Autowoot off. Ok? Ok.',false);
+  }
+
+}
+
+function DJ_ADVANCE_LISTENER(obj) {
+    wootSong();
+}
+
+function wootSong() {
+    if (wooting) {
+        $("#woot").click();
+    }
+}
+
+function changebg(url) {
+  $('#playback .background').hide();
+  $('#playback-container').css('border', '2px solid #4D4D4D');
+  $('body').css('background-image', 'url('+url+')');
+   
+}
+
 function initAPIListeners() {
     API.on(API.CHAT, chatcallback);
+    API.on(API.DJ_ADVANCE, DJ_ADVANCE_LISTENER);
+    API.on(API.CHAT_COMMAND, commandcallback);
+
 }
