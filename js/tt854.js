ba2165dfebee9f7b7eba9f3ce33df908c3442f25
Matt Thompson <matthewaaronthompson@gmail.com>
2013-12-18 23:06:17 -0600

    bookmarklet stuff

--- js/tt854.js
+++ js/tt854.js
@@ -1,5 +1,6 @@
 initAPIListeners();
 var wooting = false;
+API.chatLog('tt854 bookmarklet loaded. hi hi. look how red this message is.',true);
 
 
 function chatcallback(data) {
@@ -56,6 +57,7 @@ function wootSong() {
 function changebg(url) {
   $('#playback .background').hide();
   $('#playback-container').css('border', '2px solid #4D4D4D');
+  $('#playback-container').css('background-image', 'url(http://i.imgur.com/wBs0unz.gif)');
   $('body').css('background-image', 'url('+url+')');
    
 }
@@ -66,3 +68,7 @@ function initAPIListeners() {
     API.on(API.CHAT_COMMAND, commandcallback);
 
 }
+
+
+
+
