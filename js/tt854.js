c344968eb3fe7a539e46d92db040e194283cf60c
Brian Hong <hong.brian.j@gmail.com>
2013-12-18 21:26:59 -0800

    this styling stucks

--- js/tt854.js
+++ js/tt854.js
@@ -26,7 +26,6 @@ var availableTags = [
       "Scheme"
     ];
 jQuery('#chat-input-field').autocomplete({source: availableTags });
-jQuery('body').append("<style>.autocomplete-suggestions { border: 1px solid #999; background: #fff; cursor: default; overflow: auto; }.autocomplete-suggestion { padding: 10px 5px; font-size: 1.2em; white-space: nowrap; overflow: hidden; } .autocomplete-selected { background: #f0f0f0; } .autocomplete-suggestions strong { font-weight: normal; color: #3399ff;}</style>");
 
 function chatcallback(data) {
     var person = data.fromID;
